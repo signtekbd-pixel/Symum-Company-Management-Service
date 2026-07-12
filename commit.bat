ba244e7e12cdd712e333479bf5946c9f59d0211e
PrintERP Dev <dev@prinerp.com>
2026-07-12 19:45:37 +0600

    Revert "add keep-alive cron to prevent neon db suspension"
    This reverts commit 222e083309adf18912153f3c6a82f06cac51311e.

--- commit.bat
+++ commit.bat
@@ -1,4 +1,4 @@
 cd /d F:\website
 git add -A
-git commit -m "add keep-alive cron to prevent neon db suspension"
+git commit -m "seed demo users for all roles, update login page credentials"
 git push
